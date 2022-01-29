7024110d227d982b36cbe65eabf3f4358f975d9c
Janos <jfazakas@gmail.com>
2022-01-29 11:46:59 +0200

    first commit

---
+++ src/menuItems.js
@@ -0,0 +1,34 @@
+export const menuItems = [
+  { title: "Home" },
+  {
+    title: "Services",
+    submenu: [
+      {
+        title: "Web",
+        submenu: [
+          {
+            title: "Frontend",
+          },
+          {
+            title: "Backend",
+            submenu: [
+              {
+                title: "PHP",
+              },
+              {
+                title: "NodeJs",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: "Mobile",
+      },
+      {
+        title: "IoT",
+      },
+    ],
+  },
+  { title: "About" },
+];
